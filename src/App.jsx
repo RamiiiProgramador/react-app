0c30787a9474b1a38a121c69a48ac24f626f1f0b
RamiiiProgramador <ramirocuellarpineda@gmail.com>
2026-04-22 21:08:27 -0400

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Hola soy Ramiro</h1>
+      <h2>Ramiro Cuellar, 18 años</h2>
+      <p>River me está sacando canas</p>
+    </div>
+ 
+
+
+
+  )
+}
+
+export default App
