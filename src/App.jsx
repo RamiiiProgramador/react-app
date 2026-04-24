2380a484f0c2ba93e38d84300dbfd243b9d3f7b5
RamiiiProgramador <ramirocuellarpineda@gmail.com>
2026-04-24 17:42:22 -0400

    react-app2

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,24 @@ function App() {
 
   return (
     <div>
-      <h1>Hola soy Ramiro</h1>
-      <h2>Ramiro Cuellar, 18 años</h2>
-      <p>River me está sacando canas</p>
+      <h1>Ramiro Cuellar</h1>
+      <h2>18 años - Puerto Varas</h2>
+      <p>River me está sacando canas, me gusta tomar mate y llorar por fútbol</p>
+
+      <p>...</p>
+
+      <section>
+        <h2>Cosas organizadas</h2>
+        <p>Me gustan las facturas</p>
+        <p>Soy Argentino</p>
+        <p>Quiero vivir en Suiza</p>
+        <ul>
+          <li>Facturas</li>
+          <li>Ñoquis</li>
+          <li>Mate</li>
+        </ul>
+      </section>
     </div>
- 
 
 
 
